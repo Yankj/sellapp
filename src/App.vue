90b8a6c7645a2f20c737c159439ec790e8cf05b5
Yankj <yan.kunjie@qq.com>
2017-02-28 21:24:21 +0800

    first push

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <v-header></v-header>
+    <div class="tab tab-item">商品</div>
+    <div class="tab tab-item">评论</div>
+    <div class="tab tab-item">商家</div>
+  </div>
+</template>
+
+<script>
+  import header from './components/header/header.vue'
+  export default {
+    name: 'app',
+    components: {
+      'v-header': header
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  #app
+    .tab
+      display flex
+</style>
